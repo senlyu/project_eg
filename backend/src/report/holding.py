f8e3768066ae3126a248168d7152994c707d32ec
senlyu <me@senlyu.com>
2025-01-01 10:59:27 +0000

    better logging

--- backend/src/report/holding.py
+++ backend/src/report/holding.py
@@ -39,7 +39,7 @@ class HoldingReporting(ReportingBase):
 
         for ticker, volume in total_volume.items():
             is_option = any(char.isdigit() for char in ticker)
-            price = all_prices[ticker] * 100.0 if is_option else 1
+            price = all_prices[ticker] * (100.0 if is_option else 1)
             print(is_option, ticker, price, volume)
             self.report(ticker, volume * all_prices[ticker])
 
