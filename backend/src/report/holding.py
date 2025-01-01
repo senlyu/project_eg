2454d4124a89e6ebd8aafbbc68777717972c27e2
senlyu <me@senlyu.com>
2025-01-01 11:03:53 +0000

    better logging

--- backend/src/report/holding.py
+++ backend/src/report/holding.py
@@ -40,7 +40,7 @@ class HoldingReporting(ReportingBase):
         for ticker, volume in total_volume.items():
             is_option = any(char.isdigit() for char in ticker)
             price = all_prices[ticker] * (100.0 if is_option else 1)
-            self.report(ticker, volume * price)
+            self.report(ticker, round(volume * price, 2))
 
         
         self.report("-" * 10 + " open postions for all holding finished" + "-" * 10)
